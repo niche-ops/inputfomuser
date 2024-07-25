d9994c3ce068bbafd4d9048f374ba3084b4a4dd7
Sivalingareddymaruri <108017045+Sivalingareddymaruri@users.noreply.github.com>
2024-07-25 10:40:56 +0530

    Add files via upload

---
+++ iofromuser.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class iofromuser {
+    public static void main(String [] args)
+    {
+        double num;
+        Scanner n = new Scanner(System.in);
+        num = n.nextDouble();
+        System.out.println("The enterd double number is"+num);
+        n.close();
+    }
+    
+}
